d6be15e035f0e39c5e662b071b85369585a6c251
VADER CMU <vader.v2.1317@gmail.com>
2025-03-15 17:42:12 -0400

    add coarse segmentation code

--- src/pose_estimation/scripts/fruit_coarse_pose.py
+++ src/pose_estimation/scripts/fruit_coarse_pose.py
@@ -3,39 +3,56 @@
 import rospy
 from sensor_msgs.msg import PointCloud2, Image
 from geometry_msgs.msg import PoseStamped
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+import sensor_msgs.point_cloud2 as pc2
+from pose_estimation import PoseEstimation
+from segmentation import Segmentation
 
 class FruitDetectionNode:
     def __init__(self):
-        # Initialize the ROS node
-        rospy.init_node('fruit_detection_node', anonymous=True)
-        
-        # Store the latest messages from each topic
+
+        rospy.init_node('fruit_coarse_pose', anonymous=True)
+
         self.latest_pointcloud = None
         self.latest_image = None
         
-        # Create publisher for fruit pose
         self.pose_publisher = rospy.Publisher('fruit_pose', PoseStamped, queue_size=10)
-        
-        # Create subscribers for the two input topics
+
         rospy.Subscriber('/camera/depth/color/points', PointCloud2, self.pointcloud_callback)
         rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback)
-        
-        # Processing rate (10 Hz)
+
         self.rate = rospy.Rate(10)
         
         rospy.loginfo("Fruit detection node initialized")
     
     def pointcloud_callback(self, msg):
-        # Store the latest pointcloud message
         self.latest_pointcloud = msg
-        rospy.logdebug("Received pointcloud data")
+        pointcloud = pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True)
+        points = np.array(list(pointcloud))
+        self.latest_pointcloud = points
+
     
     def image_callback(self, msg):
-        # Store the latest image message
+
         self.latest_image = msg
-        rospy.logdebug("Received image data")
+        self.bridge = CvBridge()
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            image_np = np.array(cv_image)
+            rospy.logdebug("Converted image to numpy array")
+        except CvBridgeError as e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
+        
+        self.latest_image = image_np
+       
     
     def process_data_and_publish(self):
+
+        PoseEst = PoseEstimation()
+        model_path = rospy.get_param('weights_path')
+        Seg = Segmentation(model_path) 
         # Main processing loop
         while not rospy.is_shutdown():
             # Check if we have received both messages
@@ -47,20 +64,29 @@ class FruitDetectionNode:
                 pose_msg.header.stamp = rospy.Time.now()
                 pose_msg.header.frame_id = "camera_frame"  # Use appropriate frame ID
                 
-                # Here you would implement fruit detection algorithm
-                # to identify fruits in the image and get their 3D positions from pointcloud
-                # This is a placeholder for the actual fruit detection logic
                 
-                # Example pose (would be replaced with actual detected positions)
-                pose_msg.pose.position.x = 0.1
-                pose_msg.pose.position.y = 0.2
-                pose_msg.pose.position.z = 0.3
+                results = Seg.infer(self.latest_image)
+                result = results[0]
+                masks = result.masks
+                masks.data[0].cpu().numpy()
+                print("masks: ", masks.shape)
+
+
+                # position, quaternion = PoseEst.coarse_fruit_pose_estimation(
+                #                             self.latest_pointcloud, 
+                #                             self.latest_image
+                #                         )
+                position = np.array([0.0, 0.0, 0.0])
+    
+                pose_msg.pose.position.x = position[0]
+                pose_msg.pose.position.y = position[1]
+                pose_msg.pose.position.z = position[2]
                 
                 # Set orientation (identity quaternion in this example)
-                pose_msg.pose.orientation.x = 0.0
+                pose_msg.pose.orientation.x = 1.0
                 pose_msg.pose.orientation.y = 0.0
                 pose_msg.pose.orientation.z = 0.0
-                pose_msg.pose.orientation.w = 1.0
+                pose_msg.pose.orientation.w = 0.0
                 
                 # Publish the pose
                 self.pose_publisher.publish(pose_msg)
--- src/pose_estimation/scripts/pose_estimation.py
+++ src/pose_estimation/scripts/pose_estimation.py
@@ -4,28 +4,48 @@ import cv2
 import open3d as o3d
 from PIL import Image, ImageDraw
 import numpy as np
+import yaml
 import matplotlib.pyplot as plt
 
 class PoseEstimation:
     def __init__(self):
-        pass
+        # with open('src/pose_estimation/config/gripper_camera.yaml', 'r') as file:
+        #     config = yaml.safe_load(file)
 
-    def coarse_fruit_pose_estimation(self, rgb_image, depth_image, mask, intrinsics):
+        # self.fx = config['camera_intrinsics']['fx']
+        # self.fy = config['camera_intrinsics']['fy']
+        # self.cx = config['camera_intrinsics']['cx']
+        # self.cy = config['camera_intrinsics']['cy']
+
+        self.fx = 429.0426025390625
+        self.fy = 428.5399475097656
+        self.cx = 417.195068359375
+        self.cy = 245.24171447753906
+
+        self.intrinsics = np.array([
+            [self.fx, 0,  self.cx],
+            [0,  self.fy, self.cy],
+            [0,  0,  1]
+        ])
+
+    def coarse_fruit_pose_estimation(self, full_point_cloud, mask):
         """
         Coarse fruit pose estimation gives the position and orientation of the fruit in the camera frame
         Args: rgb_image (np.ndarray): RGB image of size (640, 480, 3)
               depth_image (np.ndarray): Depth image of size (640, 480)
               mask (np.ndarray): A singular mask of a detected target fruit
-              intrinsics (dictionary): Camera intrinsics matrix []
         Returns: position (np.ndarray): Position of the fruit in the camera frame
                  quaternion (np.ndarray): Orientation of the fruit in the camera frame
         """
 
-        # Placeholder implementation for quaternion
+        mask_pcd = self.unproject(full_point_cloud, mask)
+        pts = np.asarray(mask_pcd.points)
+        mean_x, mean_y, mean_z = pts.mean(axis=0)
 
-        # Placeholder implementation for position
-        position = np.array([0.0, 0.0, 0.0])
-        # A quaternion is represented as [w, x, y, z]
+
+
+        position = np.array([mean_x, mean_y, mean_z])
+
         quaternion = np.array([1.0, 0.0, 0.0, 0.0])
         return position, quaternion
     
@@ -48,6 +68,25 @@ class PoseEstimation:
         quaternion = np.array([1.0, 0.0, 0.0, 0.0])
         return position, quaternion
     
+    def unproject(self, points, mask):
+
+        mask = np.fliplr(mask)
+        colors = []
+        new_points = []
+        for i,point in enumerate(points):
+            x,y,z = point
+            u = mask.shape[1] - 1 - int((x * self.intrinsics[0, 0] / z) + self.intrinsics[0, 2])
+            v = int((y * self.intrinsics[1, 1] / z) + self.intrinsics[1, 2])   
+            if 0 <= u < mask.shape[1] and 0 <= v < mask.shape[0]:
+                if mask[v,u]:
+                    # colors[i] = [1,0,0]
+                    new_points.append(point)
+                    colors.append(points[i])
+
+        mask_pcd = o3d.geometry.PointCloud()
+        mask_pcd.points = o3d.utility.Vector3dVector(new_points)
+        mask_pcd.colors = o3d.utility.Vector3dVector(colors)
+        return mask_pcd
 
 class PriorityPolicy:
     def __init__(self):
